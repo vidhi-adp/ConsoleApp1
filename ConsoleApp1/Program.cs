c0d9aa63f45c7d36c7eaa4fa09b2dc14dd2cd08d
Vidhi Jadav <vjadav@mail.usf.edu>
2020-01-21 01:06:42 -0500

    Committed

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Device name: " + Environment.MachineName);
+            Console.WriteLine("64 bit system: " + Environment.Is64BitOperatingSystem);
+            Console.WriteLine("User name: " + Environment.UserName);
+            Console.ReadLine();
+        }
+    }
+}
